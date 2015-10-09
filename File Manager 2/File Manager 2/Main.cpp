6bd25b063b2a80df337899c9043526b9de2e67bb
Petoovee <Petomatick@gmail.com>
2015-10-09 18:00:50 +0200

    Variables added

--- File Manager 2/File Manager 2/Main.cpp
+++ File Manager 2/File Manager 2/Main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-#include <unistd.h>
+#include <string>
+using namespace std;
+
+int Junction(SourceFile, TargetFile)
+{
+	MovedCounter
+}
 
 int main()
 {
-	system("Hello")
+	string SourceFolder;
+	string TargetFolder;
+	int MaxSize;
+	string SourceFiles;
+	string TargetFiles;
+	int MovedCounter;
+
+	cout << "From which folder do you wish me to move the files?" << endl;
+	cin >> SourceFolder;
+	cout << endl << "To which folder do you wish me to move the files?" << endl;
+	cin >> TargetFolder;
+	cout << endl << "Minimum size (MB) of file to move?" << endl;
+ 	system("Hello");
+
+
+	return 0;
 }
